2784bddf2d3e96bbd864fd9ea95842dba17ee7c7
ruslanaliyev1991 <ruslaneliyev747@gmail.com>
2025-06-13 14:32:04 +0400

    First commit: Pl/sql file create

---
+++ Central_Bank_of_Azerbaijan.sql
@@ -0,0 +1,41 @@
+/**** Create ACL ****/
+begin
+    DBMS_NETWORK_ACL_ADMIN.create_acl(
+      acl         => 'allow_cbar_access.xml',
+      description => 'Allow web access for XML HTTP calls',
+      principal   => 'SCOTT',
+      is_grant    => TRUE,
+      privilege   => 'connect',
+      start_date => null,
+      end_date => null
+    );
+    
+    DBMS_NETWORK_ACL_ADMIN.assign_acl(
+        acl  => 'allow_cbar_access.xml',
+        host => 'www.cbar.az'
+    );
+    commit;
+end;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
